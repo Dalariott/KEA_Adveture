121794b28e0f90b01ab99cdde7ac3c857f3d896c
KEA <onbucho@gmail.com>
2021-12-16 15:40:51 -0600

    Update Push Dec16_2021 340
    Enemy chase system is working as expected

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    NavMeshAgent navAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navAgent.SetDestination(player.position);
+    }
+}
